2788bb8e4253dd55a3c7393aa0db772976284961
Ruby Jha <rubyjha@gmail.com>
2020-01-19 22:24:12 -0500

    upgraded bootstrap to 4.4.1

--- projects/mat-fitness-tracker-lib/src/lib/material.module.ts
+++ projects/mat-fitness-tracker-lib/src/lib/material.module.ts
@@ -1,5 +1,12 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatCheckboxModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatDatepickerModule,
+  MatCheckboxModule
+} from '@angular/material';
 import { MatMomentDateModule } from '@angular/material-moment-adapter';
 
 
